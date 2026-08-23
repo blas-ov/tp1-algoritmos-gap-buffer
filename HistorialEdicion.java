5c17e6856ce38fed4ab8f7d0188fc5d5cec26e25
blas-ov <oviedoblas0@gmail.com>
2026-08-23 18:22:35 -0300

    Update HistorialEdicion.java

--- HistorialEdicion.java
+++ HistorialEdicion.java
@@ -14,7 +14,32 @@ public class HistorialEdicion {
         pilaRehacer = new PilaES<Comando>();
     } // <-> end HistorialEdicion constructor
 
+    public void ejecutar(Comando c){
+        /*
+            Ejecuta el comando recibido y lo apila en Deshacer, luego vacia la pila Rehacer ya que al ejecutar un nuevo comando el futuro que estaba
+            en deshacer ya no es valido.
+        */
+        c.ejecutar();
 
+        pilaDeshacer.apilar(c);
+
+        pilaRehacer = new PilaES<>();
+    }
+
+    public boolean deshacer(Comando c){
+        /*
+            Deshace el comando ingresado (solamente si hay algo por deshacer en la pila deshacer), esto se apila en rehacer para luego poder usarlos con ctrl + z.
+            Retorna falso si no hay nada por deshacer.
+        */
+        if(sizeDeshacer() != 0){ 
+            return false;
+        }
+
+        
+
+        
+        
+    }
 
     public boolean rehacer () {
         /*
@@ -29,8 +54,11 @@ public class HistorialEdicion {
         return false;
     } // <-> end rehacer method
 
-
-
+    public int sizeDeshacer (){
+        // retornaa cantidad de elementos que contiene la pila deshace
+        return pilaDeshacer.size();
+    }
+    
     public int sizeRehacer () {
         /* retorna la cantidad de elementos que contiene pilaCtrlY */
         return pilaRehacer.size();
